026b4589bbe7daefea8c1c511d3b514e62a6545e
3848531 <3848531@U701344.univali.br>
2017-03-31 19:43:03 -0300

    commit maroto

---
+++ src/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b;
+		int soma;
+		
+		a = 10;
+		b = 20;
+		
+		soma = a+b;
+		
+		System.out.println("A soma de" + a + " + " + b + " é igual a " +soma);
+		
+	}
+
+}
