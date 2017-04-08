6afff8f0377a984f9b2ffd8655ea4d599dc0bb76
3848531 <3848531@U707902.univali.br>
2017-04-08 10:29:46 -0300

    exericicio5

--- src/exericicio5/exercicio5.java
+++ src/exericicio5/exercicio5.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import br.ufsc.ine.leb.sistemaBancario.Agencia;
+import br.ufsc.ine.leb.sistemaBancario.Banco;
+import br.ufsc.ine.leb.sistemaBancario.Moeda;
 import br.ufsc.ine.leb.sistemaBancario.SistemaBancario;
 
 public class exercicio5 {
@@ -16,6 +19,51 @@ public class exercicio5 {
 		assertEquals(true, sistemaBancario.obterBancos());
 		
 	}
+	
+	@Test
+	public void verificaNomeBancoDoBrasil() throws Exception{
+		
+		SistemaBancario sistemaBancario = Auxiliar.sistemaBancario();
+		Banco bancoDoBrasil = Auxiliar.bancoDoBrasil(sistemaBancario);
+		
+		assertEquals("Banco do Brasil", bancoDoBrasil.obterNome());
+		//assertEquals("Banco do Brasil", Auxiliar.bancoDoBrasil(sistemaBancario).obterNome());
+		
+	}
+	
+	@Test
+	public void verificaMoedaBancoDoBrasil() throws Exception{
+		
+		SistemaBancario sistemaBancario = Auxiliar.sistemaBancario();
+		Banco bancoDoBrasil = Auxiliar.bancoDoBrasil(sistemaBancario);
+		
+		assertEquals(Moeda.BRL, bancoDoBrasil.obterMoeda());
+		
+	}
+	
+	@Test
+	public void verificaNomeAgencia() throws Exception{
+		
+		SistemaBancario sistemaBancario = Auxiliar.sistemaBancario();
+		Banco bancoDoBrasil = Auxiliar.bancoDoBrasil(sistemaBancario);
+		Agencia centro = Auxiliar.agenciaCentro(bancoDoBrasil);
+		
+		assertEquals("Centro", centro.obterNome());
+		
+	}
+	
+	@Test
+	public void verificaCodigoAgencia() throws Exception{
+		
+		SistemaBancario sistemaBancario = Auxiliar.sistemaBancario();
+		Banco bancoDoBrasil = Auxiliar.bancoDoBrasil(sistemaBancario);
+		Agencia centro = Auxiliar.agenciaCentro(bancoDoBrasil);
+		
+		assertEquals("001", centro.obterIdentificador());
+		
+	}
+	
+	
 	
 	
 }
